4fa283aaa117c350773a15a6c82cad39aa93e5e7
PowderK <powderk@arcor.de>
2025-11-02 10:37:34 +0100

    Build/TS: tsconfig an Projektstruktur angepasst; CSS-Module-Typen in SettingsMenu referenziert

--- renderer/components/SettingsMenu.tsx
+++ renderer/components/SettingsMenu.tsx
@@ -1,3 +1,4 @@
+/// <reference path="../types/cssmodule.d.ts" />
 import React, { useState, useEffect } from 'react';
 import ImportYearTable from './ImportYearTable';
 import SettingsImportExport from './SettingsImportExport';
