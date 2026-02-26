8d3b9a3f1c5917af86d2828714472de66af2ece0
PowderK <powderk@arcor.de>
2026-02-26 09:23:57 +0100

    Build 1173: automated bump and CHANGELOG entry

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Build 1173 - 2026-02-26
+
+- automated build
+
 ## Build 1172 - 2026-02-26
 
 - automated build
--- renderer/buildInfo.ts
+++ renderer/buildInfo.ts
@@ -1,5 +1,5 @@
 // buildInfo.ts
 export const BUILD_INFO = {
   version: '1.0.3 RC',
-  build: 1172
+  build: 1173
 };
