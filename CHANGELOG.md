8a25e246d764b378157e8049e4adf772487878c7
PowderK <powderk@arcor.de>
2026-06-01 10:22:33 +0200

    Build 1398: automated bump and CHANGELOG entry

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Build 1398 - 2026-06-01
+
+- automated build
+
 ## Build 1397 - 2026-06-01
 
 - automated build
--- renderer/buildInfo.ts
+++ renderer/buildInfo.ts
@@ -1,5 +1,5 @@
 // buildInfo.ts
 export const BUILD_INFO = {
   version: '1.0.3 RC',
-  build: 1397
+  build: 1398
 };
