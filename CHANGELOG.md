3a49aa39d880680bba91c964aa788e72a0eb6a12
PowderK <powderk@arcor.de>
2026-09-22 14:13:29 +0200

    Build 1508: automated bump and CHANGELOG entry

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Build 1508 - 2026-09-22
+
+- automated build
+
 ## Build 1507 - 2026-09-22
 
 - automated build
--- renderer/buildInfo.ts
+++ renderer/buildInfo.ts
@@ -1,5 +1,5 @@
 // buildInfo.ts
 export const BUILD_INFO = {
   version: '1.0.3 RC',
-  build: 1507
+  build: 1508
 };
